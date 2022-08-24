55edc826039dffe87340850db0ada1f78599850b
Heeehan <ianmonash10@gmail.com>
2022-08-25 03:51:41 +0800

    eslint

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,8 +1,8 @@
-// eslint-disable-next-line react/display-name
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
+// eslint-disable-next-line react/display-name
 const Thumbnail = forwardRef( ({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
@@ -30,4 +30,4 @@ const Thumbnail = forwardRef( ({ result }, ref) => {
 })
 
 
-export default forwardRef
+export default Thumbnail
